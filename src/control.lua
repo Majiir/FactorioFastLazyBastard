8c23d9e6b0c3bbedea144a6a7bf3eb0c04aa6ebc
Daniel Zlotin <danielzl@wix.com>
2018-01-04 17:16:13 +0200

    fix

--- src/control.lua
+++ src/control.lua
@@ -1,4 +1,4 @@
-script.on_event([defines.events.on_player_created, defines.on_player_joined_game, defines.on_force_created], function(event)
+script.on_event({defines.events.on_player_created, defines.on_player_joined_game, defines.on_force_created}, function(event)
   game.forces["player"].manual_mining_speed_modifier=2
   game.forces["player"].worker_robots_speed_modifier=1.5
 end)

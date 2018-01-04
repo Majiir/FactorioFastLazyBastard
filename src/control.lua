79a5cec97375a42e6fce20a7507fe8518b511e48
Daniel Zlotin <danielzl@wix.com>
2018-01-04 17:10:37 +0200

    update

--- src/control.lua
+++ src/control.lua
@@ -1,5 +1,4 @@
-script.on_event(defines.events.on_player_created, function(event)
-	local player = game.players[event.player_index]
-	player.force.manual_mining_speed_modifier=2
-	player.force.worker_robots_speed_modifier=1.5
+script.on_event([defines.events.on_player_created, defines.on_player_joined_game, defines.on_force_created], function(event)
+  game.forces["player"].manual_mining_speed_modifier=2
+  game.forces["player"].worker_robots_speed_modifier=1.5
 end)

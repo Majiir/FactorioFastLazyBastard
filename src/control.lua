33e18a1c0d0072c9e5d49ea6bf651d4042cc1062
Daniel Zlotin <danielzl@wix.com>
2018-01-08 09:57:47 +0200

    update

--- src/control.lua
+++ src/control.lua
@@ -1,4 +1,14 @@
 script.on_event({defines.events.on_player_created, defines.on_player_joined_game, defines.on_force_created}, function(event)
   game.forces["player"].manual_mining_speed_modifier=2
   game.forces["player"].worker_robots_speed_modifier=1.5
+  -- set_allows_action(action, boolean)
+  game.forces["player"].print(defines.input_action)
+  game.forces["player"].print(game.permissions.get_group("Default"))
+end)
+
+script.on_event(defines.events.on_player_created, function(event)
+  local player = game.players[event.player_index]
+	player.insert{name="assembling-machine-3", count=1}
+  player.insert{name="small-electric-pole", count=2}
+  player.insert{name="solar-panel", count=1}
 end)

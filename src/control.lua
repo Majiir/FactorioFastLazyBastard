b129971f5e9337ba4c37bb2c23bcacfc2a0b9e12
Daniel Zlotin <danielzl@wix.com>
2018-01-08 10:34:31 +0200

    fixed wrong initialization

--- src/control.lua
+++ src/control.lua
@@ -1,14 +1,15 @@
-script.on_event({defines.events.on_player_created, defines.on_player_joined_game, defines.on_force_created}, function(event)
-  game.forces["player"].manual_mining_speed_modifier=2
-  game.forces["player"].worker_robots_speed_modifier=2
-  game.forces["player"].character_build_distance_bonus=60
-  game.forces["player"].character_reach_distance_bonus=60
-  game.permissions.get_group("Default").set_allows_action(defines.input_action.craft, false)
-end)
-
 script.on_event(defines.events.on_player_created, function(event)
   local player = game.players[event.player_index]
+
+  player.force.manual_mining_speed_modifier=2
+  player.force.worker_robots_speed_modifier=2
+
+  player.character_build_distance_bonus=60
+  player.character_reach_distance_bonus=60
+
 	player.insert{name="assembling-machine-3", count=1}
   player.insert{name="small-electric-pole", count=2}
   player.insert{name="solar-panel", count=1}
+
+  game.permissions.get_group("Default").set_allows_action(defines.input_action.craft, false)
 end)

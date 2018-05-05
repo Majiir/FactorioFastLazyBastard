d6a9cfe2587a69c261a53ca1da382348245a5adc
Daniel Zlotin <zlotindaniel@gmail.com>
2018-05-05 05:30:40 +0300

    Update control.lua

--- src/control.lua
+++ src/control.lua
@@ -1,15 +1,15 @@
 script.on_event(defines.events.on_player_created, function(event)
   local player = game.players[event.player_index]
 
-  player.force.manual_mining_speed_modifier=2
-  player.force.worker_robots_speed_modifier=2
+  player.force.manual_mining_speed_modifier=3
+  player.force.worker_robots_speed_modifier=3
 
-  player.character_build_distance_bonus=60
-  player.character_reach_distance_bonus=60
+  player.character_build_distance_bonus=100
+  player.character_reach_distance_bonus=100
 
 	player.insert{name="assembling-machine-3", count=1}
-  player.insert{name="small-electric-pole", count=2}
-  player.insert{name="solar-panel", count=1}
+	player.insert{name="small-electric-pole", count=1}
+	player.insert{name="solar-panel", count=1}
 
   game.permissions.get_group("Default").set_allows_action(defines.input_action.craft, false)
 end)

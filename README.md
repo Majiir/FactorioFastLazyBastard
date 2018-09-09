1dc62b90ad02d133a610efd411fbeff72507839a
Daniel Zlotin <zlotindaniel@gmail.com>
2018-09-09 19:00:23 +0300

    less cheaty

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 For very lazy bastards.
 - Disables hand crafting for all players
 - Start the game with 1 Electric Poll, 1 Solar Panel, 1 Assembling Machine-3
-- Faster mining and deconstruction
-- Faster default construction robot speed.
+- Faster default construction robot speed
 - Longer reach
 - Better pollution visibility on map
--- src/control.lua
+++ src/control.lua
@@ -1,10 +1,3 @@
-script.on_init(function ()
-  applyMod()
-end)
-
-script.on_configuration_changed(function ()
-  applyMod()
-end)
 
 script.on_event(defines.events.on_runtime_mod_setting_changed, function ()
   applyMod()
@@ -16,20 +9,11 @@ script.on_event(defines.events.on_player_created, function (event)
     player.insert{name="assembling-machine-3", count=1}
     player.insert{name="small-electric-pole", count=1}
     player.insert{name="solar-panel", count=1}
+
+    player.character_build_distance_bonus=100
+    player.character_reach_distance_bonus=100
+    player.force.worker_robots_speed_modifier=5
   end
 
   game.permissions.get_group("Default").set_allows_action(defines.input_action.craft, false)
-
-  applyMod()
 end)
-
-function applyMod()
-  for _, player in pairs(game.players) do
-    if player and player.character then
-      player.character_build_distance_bonus=100
-      player.character_reach_distance_bonus=100
-      player.force.manual_mining_speed_modifier=5
-      player.force.worker_robots_speed_modifier=5
-    end
-  end
-end

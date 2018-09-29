75de898b1204fbeda6c5d76810431f94e9220586
Daniel Zlotin <zlotindaniel@gmail.com>
2018-09-29 22:37:22 +0300

    add steam engine

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FastLazyBastard
 For very lazy bastards.
 - Disables hand crafting
-- Start the game with 1 "Burner Assembler"
+- Start the game with the bare minimum needed
 - Longer reach
 - Faster walking speed
 - Better pollution visibility
--- src/control.lua
+++ src/control.lua
@@ -2,7 +2,11 @@
 script.on_event(defines.events.on_player_created, function (event)
   local player = game.players[event.player_index]
   if player then
-    player.insert{name="assembling-machine-0", count=1}
+    player.insert{name="assembling-machine-3", count=1}
+    player.insert{name="small-electric-pole", count=1}
+    player.insert{name="offshore-pump", count=1}
+    player.insert{name="boiler", count=1}
+    player.insert{name="steam-engine", count=1}
 
     player.character_build_distance_bonus=100
     player.character_reach_distance_bonus=100

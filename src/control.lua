9b4bf510a9e92a93aa5cc78b78fc83d34043afec
Majiir Paktu <majiir@nabaal.net>
2019-05-25 11:59:35 -0400

    Remove player stat bonuses

--- src/control.lua
+++ src/control.lua
@@ -7,10 +7,6 @@ script.on_event(defines.events.on_player_created, function (event)
     player.insert{name="offshore-pump", count=1}
     player.insert{name="boiler", count=1}
     player.insert{name="steam-engine", count=1}
-
-    player.character_build_distance_bonus=100
-    player.character_reach_distance_bonus=100
-    player.character_running_speed_modifier=1
   end
 
   game.permissions.get_group("Default").set_allows_action(defines.input_action.craft, false)
